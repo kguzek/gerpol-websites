785f3915dae081a0369020fd367a867d613e3b41
Konrad (PC) <konrad@guzek.uk>
2025-03-08 22:39:18 +0100

    add dockerfile

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,5 @@
 import type { VariantProps } from "class-variance-authority";
-import * as React from "react";
+import type { ComponentProps } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
 import { Loader } from "lucide-react";
@@ -43,7 +43,7 @@ function Button({
   size,
   asChild = false,
   ...props
-}: React.ComponentProps<"button"> &
+}: ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
   } & { loading?: boolean }) {
