b769d488c2d563a685cec5fc1be680f91a40106d
Konrad (PC) <konrad@guzek.uk>
2025-03-12 19:14:50 +0100

    fix service card sizing

--- src/components/services/service-card.tsx
+++ src/components/services/service-card.tsx
@@ -5,13 +5,13 @@ import type { Service } from "@/lib/types";
 
 export function ServiceCard({ service: { image, label } }: { service: Service }) {
   return (
-    <Link href="#contact" title={label} className="w-full max-w-[420px] sm:w-full">
+    <Link href="#contact" title={label} className="w-full max-w-[420px]">
       <Image
         src={image}
         alt={label}
         width={420}
         height={220}
-        className="h-auto w-auto rounded-t-sm"
+        className="h-auto w-full rounded-t-sm"
       />
       <p className="bg-accent text-secondary line-clamp-2 rounded-b-sm px-2 py-1 text-center text-sm font-light">
         {label}
