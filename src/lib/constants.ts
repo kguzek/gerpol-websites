e585b873e1b911a1fb07fc614b823c083a0169d8
Konrad Guzek <konrad@guzek.uk>
2025-04-07 01:09:58 +0200

    remove unnecessary condition

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,8 +4,7 @@ export const EMAIL_RECIPIENT = process.env.EMAIL_RECIPIENT || EMAIL_ADDRESS;
 export const TURNSTILE_SITE_KEY = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY || "";
 export const TURNSTILE_SECRET_KEY = process.env.TURNSTILE_SECRET_KEY;
 
-export const TURNSTILE_SANDBOX_MODE_ENABLED =
-  process.env.NODE_ENV === "development" && true;
+export const TURNSTILE_SANDBOX_MODE_ENABLED = process.env.NODE_ENV === "development";
 
 export const DAS_PROFITEAM = "das-profiteam-berlin.de";
 export const TRAUM_BAD = "traum-bad-berlin.de";
