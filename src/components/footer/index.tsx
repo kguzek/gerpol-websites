0b097c646800c17a3825e4277f4955b93f9b2f25
Konrad Guzek <konrad@guzek.uk>
2025-05-20 11:58:48 +0200

    remove redundant fragment

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -18,35 +18,33 @@ export function Footer({
   ctaUrl: string;
 }) {
   return (
-    <>
-      <div id="contact" className="mt-12 px-4 sm:px-10 md:px-28">
-        <div className="text-center">
-          <p>{ctaText}</p>
-          <Button asChild variant="outline" className="my-4">
-            <Link href={ctaUrl}>{ctaLabel}</Link>
-          </Button>
-        </div>
-        <Heading>Kontakt aufnehmen</Heading>
-        <div className="mt-6 mb-10 grid grid-cols-1 gap-4 md:grid-cols-2">
-          <address className="my-4 grid grid-cols-1 gap-2 space-y-6 text-sm not-italic sm:grid-cols-2 md:flex md:flex-col">
-            <div className="flex items-center gap-1 sm:col-span-2 md:col-span-1">
-              <House className="text-accent" /> Gerpol Consulting UG (haftungsbeschränkt)
-            </div>
-            <Contact />
-          </address>
-          <ContactForm fromEmail={contactFromEmail} />
-        </div>
-        <hr className="border-accent absolute left-0 w-full" />
-        <footer className="mb-2 pt-2 text-center">
-          Erstellt mit 🤍 von{" "}
-          <Link
-            href="https://www.guzek.uk"
-            className="text-accent transition-all duration-300 hover:underline"
-          >
-            Konrad Guzek
-          </Link>
-        </footer>
+    <div id="contact" className="mt-12 px-4 sm:px-10 md:px-28">
+      <div className="text-center">
+        <p>{ctaText}</p>
+        <Button asChild variant="outline" className="my-4">
+          <Link href={ctaUrl}>{ctaLabel}</Link>
+        </Button>
       </div>
-    </>
+      <Heading>Kontakt aufnehmen</Heading>
+      <div className="mt-6 mb-10 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <address className="my-4 grid grid-cols-1 gap-2 space-y-6 text-sm not-italic sm:grid-cols-2 md:flex md:flex-col">
+          <div className="flex items-center gap-1 sm:col-span-2 md:col-span-1">
+            <House className="text-accent" /> Gerpol Consulting UG (haftungsbeschränkt)
+          </div>
+          <Contact />
+        </address>
+        <ContactForm fromEmail={contactFromEmail} />
+      </div>
+      <hr className="border-accent absolute left-0 w-full" />
+      <footer className="mb-2 pt-2 text-center">
+        Erstellt mit 🤍 von{" "}
+        <Link
+          href="https://www.guzek.uk"
+          className="text-accent transition-all duration-300 hover:underline"
+        >
+          Konrad Guzek
+        </Link>
+      </footer>
+    </div>
   );
 }
